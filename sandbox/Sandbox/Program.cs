273a8dcebf0857299a778c50eee982809f1de99f
ajbeus <aj732@byui.edu>
2025-01-22 20:04:14 -0700

    wrote resume and job classes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,44 +1,33 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        
-        string adjective = GetAdjective();
-        string noun = GetNoun();
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars"; 
+        favoriteMovie._year = 1977;
+        favoriteMovie._rating = "PG"; 
+        favoriteMovie._runtime = 150;
 
-        int number = Multiply(3, 4);
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._rating = "PG-13";
+        otherMovie._runtime = 162;
 
-        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
+        favoriteMovie.Display();
+
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
+
+        Console.WriteLine(favoriteMovie._title);
     }
-
-    static int Multiply(int number1, int number2)
+    static void DisplayMovie(Movie aMovie)
     {
-        int product = number1 * number2;
-
-        return product;
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
     }
-
-
-
-    static string GetAdjective()
-    {
-        List<string> words = new List<string>();
-        words.Add("blue");
-        words.Add("yellow");
-        words.Add("big");
-        words.Add("small");
-
-        string adjective = words[2];
-
-        return adjective;
-    }
-
-    static string GetNoun()
-    {
-        string noun = "birds";
-
-        return noun;
-    }
+    
 }
